d620526e7cdb6e9def4f6740ae33e38171462f98
LxrsV1-YT <69675999+LxrsV1-YT@users.noreply.github.com>
2022-04-22 18:07:46 +0200

    Delete components directory

--- components/defensie/HomeContainer.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "../../styles/Home.module.css";
-
-export default function HomeContainer({ children }: any) {
-  return <div className={styles.home_container}>{children}</div>;
-}
