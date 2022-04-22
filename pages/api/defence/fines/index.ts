5da21443f325b3e42d7d4ce4a7262c59620624ff
LxrsV1-YT <69675999+LxrsV1-YT@users.noreply.github.com>
2022-04-22 18:07:55 +0200

    Delete pages directory

--- pages/api/defence/fines/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { GetFines } from "../../../../util/database";
-
-const main = (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method != "GET") return; //TODO: Return response
-  const data = GetFines();
-  res.status(200).send(data);
-};
-export default main;
