7b08b92891f30976d5d224b1d01257f28766639b
LxrsV1-YT <69675999+LxrsV1-YT@users.noreply.github.com>
2022-04-22 18:08:15 +0200

    Delete util directory

--- util/darkmode.ts
+++
@@ -1,7 +0,0 @@
-export function GetDarkMode() {
-  if (JSON.parse(localStorage.getItem("__MODE_DARK__") || "")) return true;
-  else false;
-}
-export function UpdateDarkMode(enabled: boolean) {
-  localStorage.setItem("__MODE_DARK__", JSON.stringify(enabled));
-}
